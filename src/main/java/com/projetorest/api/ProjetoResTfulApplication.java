272939f2f92e8924eb0f08b2951a0e6ea6e5f030
João Vinicius <joaodessimonifernandes@outlook.com>
2023-12-17 01:57:58 -0300

    Project Start

---
+++ src/main/java/com/projetorest/api/ProjetoResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.projetorest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoResTfulApplication.class, args);
+	}
+
+}
